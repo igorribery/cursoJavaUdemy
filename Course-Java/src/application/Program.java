ce26c1d735ddc73443332a4b1d21ce811152c168
Igor <(igorrds1998@gmail.com)>
2022-05-13 14:59:22 -0300

    Inf do caminho do arquivo

--- Course-Java/src/application/Program.java
+++ Course-Java/src/application/Program.java
@@ -8,25 +8,15 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a folder path: ");
+		
+		System.out.println("Enter a file path: ");
 		String strPath = sc.nextLine();
 		
 		File path = new File(strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("Folders:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("Files:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean sucess = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created sucessfully: " + sucess);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 				
 				
 		sc.close();
